8a1a2627f581bb2f492b8f83e3fcbd5e9aa2bfb4
Azima <hades@gmail.com>
2020-02-04 23:40:08 +0700

    Tugas 1 JS
    Javascript 1 Niomic :)

---
+++ tugas1.js
@@ -0,0 +1,8 @@
+var firstname = "Aziiz ";
+let lastname = "Azima,";
+const born = 1996;
+const now = 2020;
+const age = now-born;
+
+console.log("Hallo",firstname+lastname);
+console.log("Umur Anda Sekarang",age);
